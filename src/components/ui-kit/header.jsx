b34570cb406bd6b7cbd9866fd29e7bf8066c6a20
Anchal <Anchal@flyde.in>
2025-11-07 15:20:48 +0530

    pull  form sahil for testimonial and header

--- src/components/ui-kit/header.jsx
+++ src/components/ui-kit/header.jsx
@@ -56,6 +56,7 @@ export default function Header() {
                   fontSize: "16px",
                 }}
               >
+                
                 {link.name}
               </Typography>
             </Link>
